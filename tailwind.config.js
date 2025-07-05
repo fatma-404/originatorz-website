068fa89971deb75883262452ce35ec9cb82bdb6e
Fatimaa <fatmahnoor.905@gmail.com>
2025-07-05 11:39:11 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+
+// tailwind.config.js
+module.exports = {
+  content: ["./**/*.{html,js}"], // <-- or ["./**/*.{html,js}"] if files are nested
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
